086f881a1836eb00b09c0c265556bfab83bd57f0
Karthik suryadevara <karthiksurya2022@gmail.com>
2022-08-13 09:08:59 +0530

    del fns

--- index.js
+++ index.js
@@ -4,21 +4,7 @@ function filterFunc(array) {
   return filteredArr
 }
 
-function add(a, b) {
-  return a + b
-}
-function sub(a, b) {
-  return a - b
-}
 
-
-
-
-
-
-function mult(a, b) {
-  return a * b
-}
 
 
 
